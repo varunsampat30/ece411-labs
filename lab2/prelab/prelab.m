a4ee4ed459d113a5e89cfd35d770cf864872c7ac
varunsampat30 <varunsampat30@gmail.com>
2022-03-04 09:12:07 -0500

    prelab pdfs and typo in .m file

--- lab2/prelab/prelab.m
+++ lab2/prelab/prelab.m
@@ -1,12 +1,23 @@
 %% Set up
 k = 1;
 g = 9.8;
-
-syms x1 x2 u
 %% Linearize model
 x_bar = [0.5; 0.0];
 u_bar = sqrt(g*x_bar(1)^2);
 
 lin_sys = linmod('non_linear_model', x_bar, u_bar);
 
-[num, den] = ss2tf(sys.a, sys.b, sys.c, sys.d)
+%{
+lin_sys.a = [0          1.0000]
+            [39.2000         0]
+
+lin_sys.b = [0       ]
+            [-12.5220]
+%}
+
+[num, den] = ss2tf(lin_sys.a, lin_sys.b, lin_sys.c, lin_sys.d)
+
+%{
+num = [0              0  -12.5220]
+den = [1.0000         0  -39.2000]
+%}
