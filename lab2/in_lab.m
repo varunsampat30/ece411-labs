f68997783dd65e66cd4494297f1b2cb9bc2cf3ce
varunsampat30 <varunsampat30@gmail.com>
2022-03-11 11:02:50 -0500

    Cleanup and added discrete controller

--- lab2/in_lab.m
+++ lab2/in_lab.m
@@ -1,9 +1,20 @@
 clear all
+clc
+%% Set up
+k = 1;
+g = 9.8;
 
-%% vars
+%% Linearize model
 
-A = [0          1.0000;39.2000         0];
-B = [0; -12.5220];
+x_bar = [0.5; 0.0];
+u_bar = sqrt(g*x_bar(1)^2);
+
+lin_sys     = linmod('non_linear_model', x_bar, u_bar);
+[num, den]  = ss2tf(lin_sys.a, lin_sys.b, lin_sys.c, lin_sys.d);
+
+A = lin_sys.a;
+B = lin_sys.b;
+%% Pole placement
 p_ctrl = [-1 -1.5];
 F      = -1*place(A, B, p_ctrl) % we use u = Kx instead of u = -Kx
 f1 = F(1);
@@ -17,5 +28,10 @@ d = f1 * alpha
 sys = tf([c, d],[1 alpha])
 
 %% dt sampling period
-
 T  = 0.05;
+
+%% 4.2.3 Discretize first order output controller
+discrete_controller = c2d(sys, T, 'tustin')
+[d_num,d_den,ts] = tfdata(discrete_controller);
+d_num = cell2mat(d_num);
+d_den = cell2mat(d_den);
